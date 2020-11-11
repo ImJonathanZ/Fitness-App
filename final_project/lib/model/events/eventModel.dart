4ef9645ae4e9f61435b1312667d948d0fe130174
Jonathan Zhu <jzhu257@gmail.com>
2020-11-11 14:17:43 -0500

    Set up local storage. no fuctioning calendar yet.

---
+++ final_project/lib/model/events/eventModel.dart
@@ -0,0 +1,27 @@
+import 'package:final_project/model/events/event.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:final_project/model/DBUtils.dart';
+
+class EventModel {
+  Future<void> insertGrade(Event event) async {
+    final db = await DBUtils.init();
+    db.insert(
+      'event_items',
+      event.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Event>> getAllEvents() async {
+    final db = await DBUtils.init();
+    final List<Map<String, dynamic>> maps = await db.query('event_items');
+    List<Event> result = [];
+    if (maps.length > 0) {
+      for (int i = 0; i < maps.length; i++) {
+        result.add(Event.fromMap(maps[i]));
+      }
+    }
+
+    return result;
+  }
+}
--- final_project/lib/views/show_calendar.dart
+++ final_project/lib/views/show_calendar.dart
@@ -1,5 +1,9 @@
+import 'package:final_project/model/DBUtils.dart';
+import 'package:final_project/model/events/event.dart';
+import 'package:final_project/model/events/eventModel.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:final_project/model/utils.dart';
 
 class DisplayCalendar extends StatefulWidget {
   DisplayCalendar({Key key, this.title}) : super(key: key);
@@ -10,15 +14,32 @@ class DisplayCalendar extends StatefulWidget {
   _DisplayCalendarState createState() => _DisplayCalendarState();
 }
 
+// Will show the days you went to workout within the past 30 days. (Can expand 30 days but files would get too large)
+
 class _DisplayCalendarState extends State<DisplayCalendar> {
   CalendarController _calendarController; //Part of calendar package
   Map<DateTime, List> _events; //Map of dates and events on that date
   List _selectedEvents;
+  EventModel _model = EventModel();
+  List<Event> eventList;
+  var _today = DateTime.now();
 
   @override
   void initState() {
     super.initState();
     _calendarController = CalendarController();
+    reload();
+    print(toDateString(_today));
+  }
+
+  void reload() {
+    DBUtils.init().then((_) {
+      _model.getAllEvents().then((events) {
+        setState(() {
+          eventList = events;
+        });
+      });
+    });
   }
 
   @override
