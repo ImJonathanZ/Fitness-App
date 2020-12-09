b18fd75a92fa8cc31343fe78fe1e6569c9566546
Jonathan Zhu <jzhu257@gmail.com>
2020-12-09 17:42:33 -0500

    Added maps

--- final_project/lib/views/show_map.dart
+++ final_project/lib/views/show_map.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 class ShowMap extends StatefulWidget {
   ShowMap({Key key, this.title}) : super(key: key);
@@ -10,14 +13,40 @@ class ShowMap extends StatefulWidget {
 }
 
 class _MapPage extends State<ShowMap> {
+  var _mapController = MapController();
+  var centre = LatLng(43.94595872178117, -78.89598774418127);
+  var _geolocator = Geolocator();
+  var zoomNum = 14.0;
+
   @override
   Widget build(BuildContext context) {
+    _geolocator.checkGeolocationPermissionStatus();
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Find a Fitness Facility'),
       ),
-      body: Center(),
+      body: buildMap(),
+    );
+  }
+
+  Widget buildMap() {
+    return FlutterMap(
+      options: MapOptions(
+        zoom: zoomNum,
+        minZoom: 6.0,
+        maxZoom: 18.0,
+        center: centre,
+      ),
+      mapController: _mapController,
+      layers: [
+        TileLayerOptions(
+          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          subdomains: ['a', 'b', 'c'],
+        ),
+        MarkerLayerOptions(),
+        PolylineLayerOptions(polylines: []),
+      ],
     );
   }
 }
